a7306cd90c775ea20b7d22607be4f556466e75f4
padermo <fabioaeh96@gmail.com>
2022-11-15 16:40:20 -0500

    terminando componente document

--- src/components/Document/Document.jsx
+++ src/components/Document/Document.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Document() {
+  const Data = [
+    {
+      id: "1D",
+      name: "Mozilla Developer Network",
+      url: "https://developer.mozilla.org/en-US/"
+    },
+    {
+      id: "2D",
+      name: "W3schools",
+      url: "https://www.w3schools.com/default.asp"
+    },
+    {
+      id: "3D",
+      name: "React JS",
+      url: "https://reactjs.org/"
+    },
+    {
+      id: "4D",
+      name: "Sequelize",
+      url: "https://sequelize.org/docs/v6/"
+    },
+    {
+      id: "5D",
+      name: "Npm",
+      url: "https://www.npmjs.com/"
+    },
+    {
+      id: "6D",
+      name: "Node JS",
+      url: "https://nodejs.org/en/docs/"
+    }
+  ];
+  const [viewData, setViewData] = useState();
+
+  useEffect(() => {
+    setViewData(
+      Data.map(e => (
+        <div key={e.id}>
+          <h3>{e.name}</h3>
+          <a href={e.url}>Ir a pagina.</a>
+        </div>
+      ))
+    )
+  },[])
+
   return (
     <div className='container-document'>
-
+      <div className="container-interno-document">
+        {viewData}
+      </div>
     </div>
   )
 }
